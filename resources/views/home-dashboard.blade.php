ba7beab0a1a949df161ae2332cfb27805f26e7f8
Harsa Yamani <harsayamani@gmail.com>
2023-06-24 22:19:37 +0700

    change desc

--- resources/views/home-dashboard.blade.php
+++ resources/views/home-dashboard.blade.php
@@ -8,8 +8,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Harsa Yamani and Rien Balqis Afifah Weddings</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
-	<meta name="keywords" content="free html5, free template_welcome, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="description" content="This is Our Wedding" />
+	<meta name="keywords" content="This is web invitation wedding" />
 	<meta name="author" content="FREEHTML5.CO" />
 
   <!--
