27f50fd35779d15fb9f96dbe3c6f7e601a91bf3c
Harsa Yamani <harsayamani@gmail.com>
2023-06-25 23:37:53 +0700

    fix wishes

--- resources/views/home-dashboard.blade.php
+++ resources/views/home-dashboard.blade.php
@@ -13,7 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="This is Our Wedding" />
     <meta name="keywords" content="This is web invitation wedding" />
-    <meta name="author" content="FREEHTML5.CO" />
+    <meta name="author" content="-" />
 
     <!--
     //////////////////////////////////////////////////////
